9bdfc46188b69b207ce064fd70ab9110f4c1411e
GB-Navarro <gbnavarro@usp.br>
2022-06-29 12:10:34 -0300

    A function to validate sign-up data has been created

--- server.js
+++ server.js
@@ -17,10 +17,42 @@ mongoClient.connect().then(() => {
 app.use(express.json());
 app.use(cors());
 
-//let 
+let registrationData = {
+    name:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+}
+
+const registrationDataSchema = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(5).email({minDomainSegments: 2, tlds:{allow: ['com']}}).required(),
+    password: Joi.string().min(8).required(),
+    confirmPassword: Joi.string().min(8).required()
+    //usar regex aqui (junto com o Joi) pra que as senhas criadas sejam obrigatoriamente senhas fortes
+    //usar regex pra que os nomes sejam criados apenas com caracteres
+    //estudar mais sobre validações de email e aplicar aqui
+})
 
 app.post("/sign-up", (req, res) => {
-    res.send("Hello World !");
+    let registrationData = req.body;
+    if(validateRegistrationData(registrationData)){
+        res.send("Hello World !");
+    }else{
+        console.log()
+        res.sendStatus(422);
+    }
 })
 
 app.listen(5000);
+
+function validateRegistrationData(registrationData){
+    let isValid;
+    if(registrationDataSchema.validate(registrationData).error === undefined){
+        isValid = true;
+        return isValid;
+    }else{
+        isValid = false;
+        return isValid;
+    }
+}

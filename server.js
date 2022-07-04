8588a7c104a177b0efcec2200f3ffbc25bdbfebd
GB-Navarro <gbnavarro@usp.br>
2022-07-04 16:41:29 -0300

    The code of server.js has been improved

--- server.js
+++ server.js
@@ -1,21 +1,10 @@
 import express from "express";
 import cors from "cors";
-import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
 
 import { signUp, signIn, logOut } from "./controllers/authController.js"
 import {postEntry, getEntry} from "./controllers/dataController.js"
 
-dotenv.config();
-
 const app = express();
-
-const mongoClient = new MongoClient(process.env.MONGO_URL);
-
-let db;
-mongoClient.connect().then(() => {
-  db = mongoClient.db("myWallet");
-});
 
 app.use(express.json());
 app.use(cors());
